c43f3b4a3eb5161532dbd0358c3dea5d45f7a07b
Galvazudis <galvazudis705@gmail.com>
2019-01-22 07:57:13 +0000

    Fixed Cursor

--- MTGArenaApp/Form1.Designer.cs
+++ MTGArenaApp/Form1.Designer.cs
@@ -38,7 +38,7 @@
             // listBox1
             // 
             this.listBox1.BackColor = System.Drawing.Color.PaleTurquoise;
-            this.listBox1.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.listBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.listBox1.Dock = System.Windows.Forms.DockStyle.Left;
             this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBox1.ForeColor = System.Drawing.SystemColors.InfoText;
@@ -48,7 +48,6 @@
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(381, 929);
             this.listBox1.TabIndex = 1;
-            this.listBox1.UseWaitCursor = true;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // textBox1
@@ -62,20 +61,18 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(402, 20);
             this.textBox1.TabIndex = 2;
-            this.textBox1.UseWaitCursor = true;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.pictureBox1.Location = new System.Drawing.Point(428, 38);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(488, 680);
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.UseWaitCursor = true;
             // 
             // label1
             // 
@@ -91,7 +88,6 @@
             this.label1.Size = new System.Drawing.Size(106, 37);
             this.label1.TabIndex = 4;
             this.label1.Text = "label1";
-            this.label1.UseWaitCursor = true;
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
@@ -105,11 +101,10 @@
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listBox1);
-            this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
-            this.UseWaitCursor = true;
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
